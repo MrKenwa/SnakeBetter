e218d6eca2858fac6bbbf5f1613343cb23d2c030
Maxim <korolev112263@gmail.com>
2024-03-07 20:48:36 +0300

    feat: add start & restart screen

--- SnakeBetter/main.cpp
+++ SnakeBetter/main.cpp
@@ -21,6 +21,16 @@ using namespace std;
 //	std::cout << board_size_x << " " << board_size_y;
 //}
 
+bool doYouWannaPlay() {
+	while (!_kbhit()) {
+		continue;
+	}
+	if (_kbhit()) {
+		return true;
+	}
+	return false;
+}
+
 int main() {
 	// Set cursor position and hiding cursor
 	HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
@@ -29,50 +39,61 @@ int main() {
 	structCursorInfo.bVisible = FALSE;
 	SetConsoleCursorInfo(consoleHandle, &structCursorInfo);
 
-	// Field width and height
-	size_t w = 30;
-	size_t h = 20;
+	cout << "WELCOME TO THE SNAKE'S LAIR!" << endl;
+	cout << "PRESS ANY KEY TO START THE GAME..." << endl;
+	bool startGame = doYouWannaPlay();
+	system("cls");
+	while (startGame) {
+		// Field width and height
+		size_t w = 30;
+		size_t h = 20;
 
-	// Snakes head position
-	Point startPoint = { w / 2, h / 2 };
-	Snake snake(startPoint);
+		// Snakes head position
+		Point startPoint = { w / 2, h / 2 };
+		Snake snake(startPoint);
 
-	// Initialization of apple
-	Apple eda(w, h);
-	eda.SpawnApple();
+		// Initialization of apple
+		Apple eda(w, h);
+		eda.SpawnApple();
 
-	// Init field
-	Field map(w, h);
+		// Init field
+		Field map(w, h);
 
-	// Record init
-	Record record;
+		// Record init
+		Record record;
 
-	bool gameGoing = true;
-	while (gameGoing) {
-		bool isHit = false;
-		isHit = _kbhit();
+		bool gameGoing = true;
+		while (gameGoing) {
+			bool isHit = false;
+			isHit = _kbhit();
 
-		bool isEated = snake.MoveSnake(isHit, eda);
-		if (isEated) {
-			eda.SpawnApple();
-			while (map.field[eda.y][eda.y] == '@') {
+			bool isEated = snake.MoveSnake(isHit, eda);
+			if (isEated) {
 				eda.SpawnApple();
+				while (map.field[eda.y][eda.y] == '@') {
+					eda.SpawnApple();
+				}
+			}
+
+			gameGoing = map.isGameGoing(snake);
+
+			if (gameGoing) {
+				map.Fill(snake, eda);
+				map.Show();
+				Sleep(100);
+				gotoxy();
+			}
+			else {
+				startGame = false;
+				record.SetRecord(snake.len);
+				system("cls");
+				cout << "YOU LOOOOSE!" << endl;
+				cout << "YOUR RECORD: " << record.GetRecord() << endl;
+				cout << "PRESS ANY KEY TO RESTART GAME..." << endl;
+				startGame = doYouWannaPlay();
+				system("cls");
 			}
 		}
-
-		gameGoing = map.isGameGoing(snake);
-
-		if (gameGoing) {
-			map.Fill(snake, eda);
-			map.Show();
-			Sleep(100);
-		}
-		else {
-			record.SetRecord(snake.len);
-		}
-		gotoxy();
 	}
-	system("cls");
-	cout << "YOU LOOOOSE!" << endl;
-	cout << "YOUR RECORD: " << record.GetRecord() << endl;
+	
 }
